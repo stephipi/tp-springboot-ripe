d4dc6e2ef9123d42f9e2ff7dea46cfcd19173fcf
stephipi <stephane.ripe@ipilyon.net>
2018-02-22 23:48:34 +0100

    first commit

---
+++ src/main/java/com/igs/ipi/tpspringbootripe/TpSpringbootRipeApplication.java
@@ -0,0 +1,12 @@
+package com.igs.ipi.tpspringbootripe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringbootRipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSpringbootRipeApplication.class, args);
+	}
+}
