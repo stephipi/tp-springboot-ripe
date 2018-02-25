9f6e22ecac5e178b3f68c950f18b4a2a1226e335
stephipi <stephane.ripe@ipilyon.net>
2018-02-25 11:31:40 +0100

    partie en session + correction GameModel pour afficher correctement la vue

--- src/main/java/com/igs/ipi/tpspringbootripe/controller/GameController.java
+++ src/main/java/com/igs/ipi/tpspringbootripe/controller/GameController.java
@@ -6,21 +6,33 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.igs.ipi.tpspringbootripe.model.GameModel;
 import com.igs.ipi.tpspringbootripe.service.GameService;
+import com.igs.ipi.tpspringbootripe.service.PartieEnCoursService;
 
 @Controller
 public class GameController {
 
     private GameService gameService;
+    private PartieEnCoursService partieEnCours;
 
-    public GameController(GameService gameService) {
+    public GameController(GameService gameService, PartieEnCoursService partieEnCours) {
         this.gameService = gameService;
+        this.partieEnCours = partieEnCours;
     }
     
     @RequestMapping("/game/new")
     public ModelAndView newGame() {
         GameModel gameModel = gameService.newGame();
         ModelAndView modelAndView = new ModelAndView("game");
-        modelAndView.addObject(gameModel);
+        modelAndView.addObject("game", gameModel);
         return modelAndView;
     }
+
+    @RequestMapping("/game")
+    public ModelAndView game() {
+        GameModel gameModel = partieEnCours.getGameModel();
+        ModelAndView modelAndView = new ModelAndView("game");
+        modelAndView.addObject("game", gameModel);
+        return modelAndView;
+    }
+
 }
---
+++ src/main/java/com/igs/ipi/tpspringbootripe/service/PartieEnCoursService.java
@@ -0,0 +1,20 @@
+package com.igs.ipi.tpspringbootripe.service;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+import com.igs.ipi.tpspringbootripe.model.GameModel;
+
+@Component
+@Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class PartieEnCoursService {
+	private GameModel gameModel;
+	public GameModel getGameModel() {
+	        return gameModel;
+	}
+	
+	public void setGameModel(GameModel gameModel) {
+		this.gameModel = gameModel;
+	}
+}
